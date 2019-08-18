d1452240f845a91639a045c1ed3e51e411a9bae9
Jason Díaz G <jason.rene.diaz.gonzalez@ericsson.com>
2019-08-17 18:28:20 -0600

    Added allow Authorization header for requests in CORS

--- app/config/server.config.js
+++ app/config/server.config.js
@@ -7,7 +7,7 @@ const server = {
         app.all('/*', (req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
             next();
         });
 
